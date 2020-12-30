4c297452f2111a1fab6fc6a60bdcaf94a19a3cb4
杨莫莫桑桑 <1285511723@qq.com>
2020-12-30 15:40:06 +0800

    feat: 基础代码

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav>
+      <div class="container">
+        <ul class="nav_left">
+          <li>
+            <router-link to='/'>Home</router-link>
+          </li>
+          <li>
+            <router-link to='/admin'>Admin</router-link>
+          </li>
+          <li>
+            <router-link to='/cart'>Cart</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
